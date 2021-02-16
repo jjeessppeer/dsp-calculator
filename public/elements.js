fd954ba5d37f0295c0478769fd0c18ea4ae8b609
Jesper Ericsson <jjeessppeer@gmail.com>
2021-02-16 13:32:00 +0100

    recepie row breakdowns

--- public/elements.js
+++ public/elements.js
@@ -114,17 +114,47 @@ class OutputItemElement extends HTMLLIElement {
 class ResultRowElement extends HTMLTableSectionElement {
   constructor() {
     super();
-    this.classList.add('item-row')
+    this.classList.add('item-row');
+
+    this.innerHTML = `
+      <tr class="subfactory-row">
+        <td></td>
+        <td colspan="4">
+          <table>
+          </table>
+        </td>
+      </tr>
+    `;
+
+  this.subfactory_table = this.querySelector('table');
+
+
   }
+
 
   initializeItemRow(recepie_id, rate) {
     let recepie = recepies[recepie_id];
     this.recepie_id = recepie_id;
 
+
+    // Populate input breakdown table
+    for (const item_id in recepie.items_in){
+      let row = document.createElement('tr');
+      let rate_in = recepie.items_in[item_id] * rate;
+      row.innerHTML = `
+        <td></td>
+        <td><img src="`+items[item_id].icon+`"></td>
+        <td class="pad">&times;`+formatNumber(rate_in)+`</td>
+        <td><img src="`+items[belts[SETTINGS.belt].item].icon+`"></td>
+        <td class="pad">&times;`+formatNumber(rate_in/belts[SETTINGS.belt].speed)+`</td>
+      `;
+      this.subfactory_table.appendChild(row);
+    }
+
+    // Add rows for item outputs and the recepie.
     let length = Object.keys(recepie.items_out).length;
     let i = 0;
     for (const item_id in recepie.items_out) {
-
       let items_per_s = recepie.items_out[item_id] * rate;
       if (item_id in recepie.items_in) items_per_s -= recepie.items_in[item_id] * rate;
       if (items_per_s <= 0) continue;
@@ -145,43 +175,86 @@ class ResultRowElement extends HTMLTableSectionElement {
       document.querySelectorAll('#inputItems li:not(:last-child)').forEach(element => {
         if (element.item == item_id) is_input = true;
       });
-      is_input = is_input ? 'class="is-input"' : "";
+      is_input = is_input ? ' is-input' : "";
 
       let recepie_img = recepie.icon != items[item_id].icon && recepie.type != "IMPORT" ? "<img src="+recepie.icon+"><br>" : "";
 
       let row = document.createElement('tr');
-      // <svg><use href="launch-24px.svg" /></svg>
-
     
       let link = setHashParam(window.location.href, "outputs", item_id + ',' + items_per_s);
       
-      row.innerHTML = `
-        <td>
-          <a href="`+getHash(link)+`" target="_blank">
-          <svg viewBox="0 0 24 24">
-          <use href="icon.svg#launch" />
-          </svg>
-          </a>
-        </td>
-        <td><img src="`+items[item_id].icon+`" title="`+items[item_id].name+`" `+is_input+`></td>
+      if (i == 0){
+        row.innerHTML += `
+          <td rowspan="`+length+`">
+            <a href="`+getHash(link)+`" target="_blank">
+                <svg viewBox="0 0 24 24"><use href="icons.svg#launch" /></svg>
+            </a>
+            <br>
+            <button>
+              <svg viewBox="25 0 24 24"><use href="icons.svg#arrow" /></svg>
+            </button>
+          </td>`;
+      }
+
+      row.innerHTML += `
+        <td><img class="item-icon`+is_input+`" src="`+items[item_id].icon+`" title="`+items[item_id].name+`"></td>
         <td class="pad">`+formatNumber(items_per_s)+`</td>
         <td><img src="`+items[belts[SETTINGS.belt].item].icon+`"></td>
         <td class="pad">&times; `+ formatNumber(n_belts) + `</td>`;
       
       // Add belt, machines, and power only for the first row. Row spans the full result row.
-      if (this.childNodes.length == 0){
+      if (i == 0){
+        this.querySelector('td').innerHTML = `
+          
+        `
         row.innerHTML += `
           <td rowspan="`+ length + `">`+recepie_img+`<img src="` + items[machine_item].icon + `"></td>
           <td rowspan="`+ length + `" class="pad">&times; ` + formatNumber(n_machines) + `</td>
           <td rowspan="`+ length + `">` + math.unit(power, 'kW').toString() + `</td>
         `
       }
-      this.appendChild(row);
-      this.querySelector('tr:nth-child('+(i+1)+') > td:nth-child(2) > img').addEventListener('click', event => {
+      // this.appendChild(row);
+      this.insertBefore(row, this.querySelector('.subfactory-row'));
+      this.querySelector('tr:nth-child('+(i+1)+') .item-icon').addEventListener('click', event => {
         toggleInputRow(item_id);
       });
       i++;
     }
+    this.querySelector('button').addEventListener('click', event => {
+      this.querySelector('button').classList.toggle('active');
+      this.classList.toggle('show-breakdown');
+    });
+  }
+}
+
+class SubFactoryRow extends HTMLTableRowElement {
+  constructor(){
+    super();
+    this.classList.add('subfactory-row')
+    this.classList.add('active')
+    this.innerHTML = `
+      <td></td>
+      <td>
+        <table>
+        <td><img src="icons/iron-ore.png"></td>
+        <td class="pad">&times;1.5</td>
+        <td><img src="icons/belt-1.png"></td>
+        <td class="pad">&times;1.5</td>
+        <td><img src="icons/assembler-1.png"></td>
+        <td>&times;1.5</td>
+        </table>
+      </td>  
+    `
+  }
+}
+class SubFactoryItemRow extends HTMLTableRowElement {
+  constructor(){
+    super();
+    this.classList.add('subfactory-item-row')
+    this.innerHTML = `
+      <td>
+      </td>  
+    `
   }
 }
 
@@ -189,4 +262,5 @@ class ResultRowElement extends HTMLTableSectionElement {
 customElements.define('output-item', OutputItemElement, { extends: 'li' });
 customElements.define('input-item', InputItemElement, { extends: 'li' });
 customElements.define('result-row', ResultRowElement, { extends: 'tbody' });
+customElements.define('subfactory-row', SubFactoryRow, { extends: 'tr' });
 customElements.define('item-dropdown', ItemDropdown, { extends: 'div' });
