7118c4a202ffc2b0c7defea39df30996fd0c753b
Jesper Ericsson <jjeessppeer@gmail.com>
2022-02-01 11:52:03 +0100

    added proliferator settings

--- public/elements.js
+++ public/elements.js
@@ -1,3 +1,81 @@
+
+// Generic dropdown element where different icons can be selected.
+// Each icon should have a unique identifier.
+class IconDropdown extends HTMLDivElement {
+  constructor() {
+    super();
+    this.classList.add('dropdown');
+    this.innerHTML = `
+      <div class="dropdown-content">
+        <input type="text" placeholder="Search" style="display:none">
+        <br style="display:none">
+      </div>
+      <img class="dropbtn" src="icons/rocket.png">
+      `.trim();
+
+    this.icons = {}
+    this.selected_id = -1;
+    this.dropbtn = this.querySelector(".dropbtn");
+    this.dropbtn.addEventListener('click', event => this.toggleDropdown(event));
+    this.search = this.querySelector("input");
+
+  }
+
+  addIcon(img_src, icon_id, name="", category=0) {
+    this.icons[icon_id] = {src: img_src, id: icon_id, name: name, category: category};
+    
+    // Add category sections if missing
+    let n_cats = this.querySelectorAll(".dropdown-content > div").length;
+    while (n_cats < category + 1) {
+      n_cats += 1;
+      let div = document.createElement("div");
+      this.querySelector(`.dropdown-content`).appendChild(div);
+    }
+
+    // Add the icon to the dropdown
+    let img = document.createElement('img');
+    img.src = img_src;
+    img.title = name;
+    img.dataset.item_id = icon_id;
+    img.addEventListener('click', event => this.selectItem(event.target.dataset.item_id));
+    this.querySelector(`.dropdown-content div:nth-of-type(${category+1})`).appendChild(img);
+
+    // If it was the first icon added, select it but dont brodcast event. 
+    if (Object.keys(this.icons).length == 1) {
+      this.selectItem(icon_id, false);
+    }
+  }
+
+  selectItem(icon_id, brodcast_event=true) {
+    this.dropbtn.src = this.icons[icon_id].src;
+    this.selected_id = icon_id;
+    if (!brodcast_event) return;
+    this.dispatchEvent(new Event('item-selected'));
+  }
+
+  toggleDropdown(event) {
+    this.classList.toggle('active')
+    this.search.focus();
+  }
+}
+
+class ProliferatorDropdown extends IconDropdown {
+  constructor() {
+    super();
+    this.classList.add('proliferator-dropdown');
+
+    // Add the proliferator icons
+    for (let i=0; i < proliferators.length; i++) {
+      let p = proliferators[i];
+      this.addIcon(items[p.item_id].icon, i, items[p.item_id].name, 0);
+    }
+    
+    this.addEventListener('item-selected', event => {
+      reloadResultsTable();
+    });
+  }
+}
+
 class ItemDropdown extends HTMLDivElement {
   constructor() {
     super();
@@ -208,8 +286,13 @@ class ResultRowElement extends HTMLTableSectionElement {
         row.innerHTML += `
           <td rowspan="`+ length + `">`+recepie_img+`<img src="` + items[machine_item].icon + `"></td>
           <td rowspan="`+ length + `" class="pad">&times; ` + math.format(n_machines, SETTINGS.format) + `</td>
+          <td rowspan="`+ length + `" class="pad proliferator-cell"></td>
           <td rowspan="`+ length + `">` + math.unit(power, 'kW').format(SETTINGS.format) + `</td>
         `
+        
+        let pc = row.querySelector('.proliferator-cell');
+        let proliferator_dropdown = document.createElement('div', {is: 'proliferator-dropdown'});
+        pc.appendChild(icontest)
       }
       // this.appendChild(row);
       this.insertBefore(row, this.querySelector('.subfactory-row'));
@@ -245,6 +328,7 @@ class SubFactoryRow extends HTMLTableRowElement {
     `
   }
 }
+
 class SubFactoryItemRow extends HTMLTableRowElement {
   constructor(){
     super();
@@ -262,3 +346,5 @@ customElements.define('input-item', InputItemElement, { extends: 'li' });
 customElements.define('result-row', ResultRowElement, { extends: 'tbody' });
 customElements.define('subfactory-row', SubFactoryRow, { extends: 'tr' });
 customElements.define('item-dropdown', ItemDropdown, { extends: 'div' });
+customElements.define('icon-dropdown', IconDropdown, { extends: 'div' });
+customElements.define('proliferator-dropdown', ProliferatorDropdown, { extends: 'div' });
